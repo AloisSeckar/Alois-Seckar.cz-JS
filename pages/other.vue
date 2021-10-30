0e0eb8a95c2ad02373cda36b1b145d00ac6bbd68
Alois Sečkár <ellrohir@seznam.cz>
2021-10-30 12:13:19 +0200

    filling in page contents II

--- pages/other.vue
+++ pages/other.vue
@@ -3,15 +3,28 @@
         <page-header title="Ostatní" />
         <p>Za jednu z&nbsp;mých největších slabostí pokládám neschopnost držet se pořádně jednoho tématu. Přeskakuju z&nbsp;jednoho na druhé a&nbsp;často kvůli tomu nedotáhnu věc až do konce. Pár mých aktivit však ještě stojí za představení.</p>
         <h2>Trash Hero</h2>
-        <p>Doplním co nejdřív...</p>
+        <p>28.&nbsp;září 2014 jsme s&nbsp;kolegy ze <a href="https://www.svobodni.cz/" title="svobodni.cz">Svobodných</a> uspořádali první dobrovolnický úklid v&nbsp;Krči kolem rybníka Labuť. Od té doby podobné akce pořádám a&nbsp;chodím pomáhat jiným, kteří je pořádají. V&nbsp;březnu 2019 jsem poprvé potkal lidi z&nbsp;<strong>Trash Hero</strong>, což mé úklidové dobrovolnictví pozvedlo ještě na vyšší úroveň. Odpadky se sice neustále vrací, výsledky ale vidět jsou.</p>
+        <a href="https://www.trashhero.cz/" title="Trash Hero"><img src="/logo/trash-hero.webp" class="activities logo" alt="Trash Hero"></a>
         <h2>Historie</h2>
-        <p>Doplním co nejdřív...</p>
+        <p>Spolu s&nbsp;tátou provozujeme web o&nbsp;historii naší městské části Praha-Krč - <a href="http://krc-historie.cz/" title="Stará Krč">http://krc-historie.cz/</a>. Stránky by zasloužily ještě hodně práce, ale už dnes tam najdete desítky fotografií a&nbsp;spoustu informací, které v&nbsp;podobně ucelené podobě nikde jinde neexistují. Pracuji na tom, aby <em>Stará Krč</em> jednou vyšla i&nbsp;knižně.</p>
+        <a href="http://krc-historie.cz/" title="Stará Krč"><img class="thumb" src="/stara-krc.webp" alt="Stará Krč"></a>
         <h2>Foto</h2>
-        <p>Doplním co nejdřív...</p>
+        <p>Rád fotím náhodné věci kolem sebe a&nbsp;občas se to povede.</p>
+        <ul>
+            <li><a href="https://www.instagram.com/alois.seckar/" title="Instagram">Instagram</a></li>
+            <li><a href="https://www.deviantart.com/ellrohir" title="DeviantArt">DeviantArt</a></li>
+        </ul>
+        <a href="foto-tower-in-the-lake.webp" title="Tower in the Lake"><img class="thumb" src="/foto-tower-in-the-lake.webp" alt="Tower in the Lake"></a>
+        <a href="foto-dark-side-of-the-most.webp" title="Dark Side of the Most"><img class="thumb" src="/foto-dark-side-of-the-most.webp" alt="Dark Side of the Most"></a>
+        <a href="foto-ferris-wheel.webp" title="Ferris Wheel"><img class="thumb" src="/foto-ferris-wheel.webp" alt="Ferris Wheel"></a>
         <h2>Literatura</h2>
-        <p>Doplním co nejdřív...</p>
+        <p>Čtu rád a&nbsp;celkem dost. Nejradši fantasy a&nbsp;sci-fi, ale baví mě i&nbsp;historie a&nbsp;nejrůznější místopisy. Jsem věrný tradičním knihám, ale už je doma pomalu nemáme, kam dávat. Taky se snažím docela dost psát a&nbsp;pořád ještě jsem se nevzdal snu, že jednou napíšu vlastní román.</p>
+        <p>Různé kratší povídky, co jsem měl zveřejněné na blog.cz, bohužel z&nbsp;webu zmizely. Časem snad něco znovu přidám sem.</p>
         <h2>Hudba</h2>
-        <p>Doplním co nejdřív...</p>
+        <p>Možná byste to do mě neřekli, ale poslouchám metal. Znám desítky kapel, o&nbsp;kterých jste nejspíš ani neslyšeli. Míval jsem na to blog, než ho v&nbsp;létě 2020 zrušili. Zůstal mi už jen <a href="https://www.youtube.com/channel/UCgpeZOnY0uw20pb7tXKeFyQ" title="Howling Iron Metal Blog">YouTube Channel</a>. Pokud byste si chtěli rozšířit své hudební obzory, doporučuju mé playlisty.</p>
+        <p>Kolem let 2008-2010 jsem se pokoušel i&nbsp;o&nbsp;nějakou vlastní tvorbu, možná časem najdu vhodný způsob, jak tehdejší dema prezentovat.</p>
+        <p>Taky mě kdysi bavilo stříhat na hudbu klipy z&nbsp;filmů. Do dnešních dnů přežil jediný:</p>
+        <iframe width="400" height="266" src="https://www.youtube.com/embed/szUHyvgNtjs" title="10th Trooper Down" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <page-footer />
     </div>
 </template>
@@ -19,3 +32,11 @@
 <script>
     import "@/assets/css/as.css";
 </script>
+
+<style>
+    img.thumb {
+        height: 191px;
+        width: auto;
+        margin: 2px;
+    }
+</style>
--- pages/programovani.vue
+++ pages/programovani.vue
@@ -1,13 +1,14 @@
 <template>
     <div id="content">
         <page-header title="Programování" />
-        <p>Programování se věnuju od konce základní školy. Vystudoval jsem Informatiku na FIS&nbsp;VŠE a&nbsp;od roku 2016 pracuju jako vývojář ve firmě <a href="https://atos.net/cs/ceska-republika">Atos</a>. Primárně se věnuju Java technologiím, doplňkově světu JavaScriptu a&nbsp;tvorbě webových stránek. Baví mě <a href="https://vuejs.org/">Vue.js</a> a&nbsp;odvozený framework <a href="https://v3.nuxtjs.org/">Nuxt&nbsp;3</a>, v&nbsp;němž je napsán i&nbsp;tento web.</p>
-        <a href="https://atos.net/cs/ceska-republika"><img src="/logo/atos.webp" class="activities round" alt="atos.net"></a>
-        <p>Můj životopis naleznete <a href="/cv-seckar.pdf">ZDE</a>.</p>
-        <p>Zdrojové kódy projektů se snažím dávat na svůj GitHub - <a href="https://github.com/AloisSeckar">https://github.com/AloisSeckar</a>.</p>
-        <p>Na starém hostingu <a href="http://ellrohir.mzf.cz/">http://ellrohir.mzf.cz/</a> mi běží pár PHP a JS legrácek, které jsem zatím ještě nestihl převést sem.</p>
+        <p>Programování se věnuju od konce základní školy. Vystudoval jsem Informatiku na FIS&nbsp;VŠE a&nbsp;od roku 2016 pracuju jako vývojář ve firmě <a href="https://atos.net/cs/ceska-republika" title="Atos IT Solutions and Services">Atos</a>. Primárně se věnuju světu Java technologií, doplňkově JavaScriptu a&nbsp;tvorbě webových stránek. Baví mě <a href="https://vuejs.org/" title="Vue.js">Vue.js</a> a&nbsp;odvozený framework <a href="https://v3.nuxtjs.org/" title="Nuxt 3">Nuxt&nbsp;3</a>, v&nbsp;němž je napsán i&nbsp;tento web.</p>
+        <a href="https://atos.net/cs/ceska-republika" title="Atos IT Solutions and Services"><img src="/logo/atos.webp" class="activities round" alt="atos.net"></a>
+        <p>Můj životopis naleznete <a href="/cv-seckar.pdf" title="Životopis">ZDE</a>.</p>
+        <p>Zdrojové kódy projektů se snažím dávat na svůj GitHub - <a href="https://github.com/AloisSeckar" title="Alois Sečkár GitHub">https://github.com/AloisSeckar</a>.</p>
+        <p>Na starém hostingu <a href="http://ellrohir.mzf.cz/" title="Alois Sečkár programy">http://ellrohir.mzf.cz/</a> mi běží pár PHP a JS legrácek, které jsem zatím ještě nestihl převést sem.</p>
+        <p>Nedávno jsem se přihlásil do projektu <a href="https://www.denproskolu.cz/dobrovolnik/alois-seckar" title="Den pro školu">Den pro školu</a>.</p>
+        <h2>Master Coda</h2>
         <p>Protože si myslím, že je škoda nechávat si nabyté znalosti jen pro sebe, začal jsem tvořit web o&nbsp;programování jménem <strong>Master Coda</strong>. K&nbsp;jeho obsahu uvítám jakoukoliv zpětnou vazbu.</p>
-        <p>Nedávno jsem se přihlásil do projektu <a href="https://www.denproskolu.cz/dobrovolnik/alois-seckar">Den pro školu</a>.</p>
         <page-footer />
     </div>
 </template>
